d109225dc0b50799703dccfef448b8a75d49e248
Artem Veremiyenko <temaspsxxx@gmail.com>
2019-02-19 00:54:01 +0200

    Standart library

---
+++ versioncheck.py
@@ -0,0 +1,8 @@
+import sys, warnings
+
+if sys.version_info[0] < 3:
+    warnings.warn("Для выполения этой программы необходима как минимум \
+        версия Python 3.0", 
+        RuntimeWarning)
+else:
+    print('Нормальное продолжение')
